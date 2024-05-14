059c6e1726026f85018c05dd026a7a881105acd6
Sangeethraj K P <sangeethraj3498@gmail.com>
2024-05-15 00:19:19 +0530

    Add files via upload

---
+++ MyFirstJavaClass.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class MyFirstJavaClass 
+ {
+    public static void main ( String args[]) 
+    
+    {
+        System.out.println("Hello World");
+        System.out.print("This is my First Java Program - Sangeeth Codes");
+
+        //This is a comment line
+
+    }
+ }
