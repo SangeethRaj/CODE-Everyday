a05148c0b53ba4bbe615bcc8bbf355c7d58aa46b
Sangeethraj K P <sangeethraj3498@gmail.com>
2025-04-24 23:48:33 +0530

    Update MyFirstJavaClass.java

--- MyFirstJavaClass.java
+++ MyFirstJavaClass.java
@@ -1,17 +1,9 @@
-import java.io.*;
-
 public class MyFirstJavaClass 
  {
     public static void main ( String args[]) 
     
-    {
-
-        // This is a comment line
-        
+    {        
         System.out.println("Hello World");
         System.out.print("This is my First Java Program - Sangeeth Codes");
-
-        //This is a comment line
-
     }
  }
