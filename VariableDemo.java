52514bcdcdb40e382acaddaa6ea387abf940cb27
Sangeethraj K P <sangeethraj3498@gmail.com>
2025-04-25 12:59:20 +0530

    25 April 2025

---
+++ VariableDemo.java
@@ -0,0 +1,17 @@
+public class VariableDemo 
+{  
+    public static void main( String [] args)
+    {
+        String Name = "Sangeeth Machha";
+        int Age = 27;
+        Boolean isLearning = true;
+        double Incentive = 99.99;
+        char Grade = 'A';
+
+        System.out.println( "The user name is "+Name);
+        System.out.println( "The user age is "+Age);
+        System.out.println( "Is the user is learning? "+isLearning);
+        System.out.println( "Incentive received by the user - "+Incentive);
+        System.out.println( "The user grade is "+Grade);
+    }    
+}
