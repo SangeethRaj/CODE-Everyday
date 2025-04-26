fcd93fe24cb62d09b9cf4b37059d6005997f45c5
Sangeethraj K P <sangeethraj3498@gmail.com>
2025-04-26 23:33:54 +0530

    26 April 2025

---
+++ ConditionalSwitch.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class ConditionalSwitch {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Enter a number (1 to 7) to get the day of the week");
+
+        Scanner day = new Scanner(System.in);
+
+        int a = day.nextInt();
+
+        switch (a) 
+        {
+            case 1:
+                System.out.println("It's Sunday");
+                break;
+            
+            case 2:
+                System.out.println("It's Monday");
+                break;
+
+            case 3:
+                System.out.println("It's Tuesday");
+                break;
+
+            case 4:
+                System.out.println("It's Wednesday");
+                break;
+
+            case 5:
+                System.out.println("It's Thursday");
+                break;
+
+            case 6:
+                System.out.println("It's Friday");
+                break;
+
+            case 7:
+                System.out.println("It's Saturday");
+                break;
+
+            default:
+                break;
+        }
+
+        day.close();
+    }
+    
+}
---
+++ OddOrEven.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class OddOrEven 
+{   
+    public static void main (String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        while (true)
+
+        {
+            System.out.println("Enter a number to check Even or Odd:");
+
+            int a = input.nextInt();
+
+            if (a % 2 == 0)
+                {
+                    System.out.println("This is a Even Number");
+                    System.out.println("Do you want to continue? (yes/no)");
+                }
+            else
+                {
+                    System.out.println("This is an Odd Number");
+                    System.out.println("Do you want to continue? (yes/no)");
+                }
+
+        String choice = input.next().toLowerCase();
+
+        if (choice.equals("no"))
+            {
+                System.out.println("Thank you for using this code!");
+                break;
+            }
+        }//while
+
+        input.close();
+
+    }//method
+    
+}//class
