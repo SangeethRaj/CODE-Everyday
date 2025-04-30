a7aecdac7456b86b87e470e79e8045ab974a7634
Sangeethraj K P <sangeethraj3498@gmail.com>
2025-04-30 23:33:46 +0530

    30 April 2025

---
+++ DoWhileLoop.java
@@ -0,0 +1,23 @@
+public class DoWhileLoop 
+{
+    public static void main (String[] args)
+    {
+        int i = 1;
+
+        do
+        {
+            System.out.println("The value is "+i);
+            i = i + 1;
+        } 
+        while (i <= 5);
+
+        int a = 10;
+
+        do
+        {
+            System.out.println("The value is "+a);
+            i = i + 1;
+        }
+        while (i < 5 );
+    }   
+}
